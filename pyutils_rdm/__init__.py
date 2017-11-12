79adee3f1854b0f5a307271e7e141fa1b600d515
rydamil60000g <ryandarylmills@gmail.com>
2017-11-12 01:00:22 +0000

    Working Heroku deploy on button submit.

---
+++ pyutils_rdm/__init__.py
@@ -0,0 +1,3 @@
+from .file_rdm import *
+from .heroku_rdm import *
+from .tar_zip_rdm import *
--- webform/views.py
+++ webform/views.py
@@ -1,16 +1,28 @@
 # webform/views.py
 
 # IMPORTS
+import os
+import shutil
 from flask import render_template, request, redirect, url_for, flash
 from webform import app
 from .forms import CodeForm
+from pyutils_rdm import push_from_site_to_heroku, write_lines_to_file
 
 # ROUTES
 @app.route('/', methods=['GET', 'POST'])
 def publish_code():
+    push_data = []
     form = CodeForm(request.form)
     size = { 'left': 13, 'middle': 3, 'right': 3 }
     run = request.method == 'POST'
-    if run: print('ran')
+    if run:
+        try:
+            shutil.rmtree('tmp')
+        except:
+            pass
+        os.makedirs('tmp')
+        for field in [form.left, form.middle, form.right]:
+            write_lines_to_file('tmp/' + field.label.text, str(field.data).split('\n'))
+        push_data = push_from_site_to_heroku('tmp', 'my-tiny-heroku-app')
     """Render code form"""
-    return render_template('code_entry.html', form=form, size=size, run=run)
+    return render_template('code_entry.html', form=form, size=size, run=run, push_data=push_data)
