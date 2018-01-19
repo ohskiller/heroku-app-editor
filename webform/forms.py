6f2a0d69ff61e10640f5ae8f7a12c6a6b061ba6d
Ryan Mills <ryandarylmills@gmail.com>
2018-01-19 22:37:30 +1100

    Flask 0.12, for compatibility with Python 3.

--- webform/forms.py
+++ webform/forms.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)""",
 
-"""Flask==0.7.2""",
+"""Flask==0.12""",
 
 """web: python app.py"""
 ]
